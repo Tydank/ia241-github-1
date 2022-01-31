30fa0fe006b243c05de3cda3e6959848a930e2fa
EC2 Default User <ec2-user@ip-172-31-30-74.ec2.internal>
2022-01-31 19:50:13 +0000

    my lab2.py file.

---
+++ Lab2.py
@@ -0,0 +1,25 @@
+'''
+lab 2
+'''
+
+#3.1
+
+my_name = 'Tom'
+print(my_name.upper())
+
+my_id=your_id = 123
+print(my_id)
+print (your_id)
+
+my_id_str = "123"
+print(my_id_str)
+
+#print(my_id + my_name)
+
+print(my_name + my_id_str)
+
+
+print(my_name *3)
+
+
+print('hello world. This is my first python string.'.split('.'))
---
+++ Lec2.py
@@ -0,0 +1,24 @@
+"""
+Lec 2
+This is a reigonal comment
+"""
+#print("hello world") #this is a single line comment
+
+#print(type(123))
+
+#print(type(123.))
+
+#print(type('123'))
+
+#print('hello world')
+
+#print(" Tom's cat ")
+
+#print("Hello" + "world" )
+
+my_int = 123
+my_float = 123.
+my_str = 'hello world'
+print (my_int)
+print (my_float)
+print(my_str)
